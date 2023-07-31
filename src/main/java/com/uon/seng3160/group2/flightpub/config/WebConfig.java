ccc644b3a2ff544cfdeebba5ac3b8cc2daff7aa3
Anton Lyamin <antoine.lyamin@gmail.com>
2023-07-31 21:29:17 +1000

    Setup dispatcher servlet configuration and initialization

---
+++ src/main/java/com/uon/seng3160/group2/flightpub/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.uon.seng3160.group2.flightpub.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.uon.seng3160.group2.flightpub" })
+@EnableJpaRepositories(basePackages = { "com.uon.seng3160.group2.flightpub" })
+public class WebConfig implements WebMvcConfigurer {
+
+    // Free Marker
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(false);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("classpath:/templates");
+        return configurer;
+    }
+}
