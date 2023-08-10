128f118578966ddaca547917d0cbdbf03e1236f3
Anton Lyamin <antoine.lyamin@gmail.com>
2023-08-10 18:42:19 +1000

    Move relation mappings from Id to entity

--- src/main/java/com/uon/seng3160/group2/flightpub/entity/Price.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/entity/Price.java
@@ -6,6 +6,9 @@ import com.uon.seng3160.group2.flightpub.entity.compositekey.PriceId;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -16,6 +19,21 @@ public class Price {
 
     @EmbeddedId
     PriceId priceId;
+
+    @MapsId("airlineCode")
+    @ManyToOne
+    @JoinColumn(name = "AirlineCode", referencedColumnName = "AirlineCode", columnDefinition = "CHAR(2)")
+    private Airline airline;
+
+    @MapsId("classCode")
+    @ManyToOne
+    @JoinColumn(name = "ClassCode", referencedColumnName = "ClassCode", columnDefinition = "CHAR(3)")
+    private TicketClass ticketClass;
+
+    @MapsId("ticketCode")
+    @ManyToOne
+    @JoinColumn(name = "TicketCode", referencedColumnName = "TicketCode", columnDefinition = "CHAR(1)")
+    private TicketType ticketType;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
@@ -33,10 +51,15 @@ public class Price {
     public Price() {
     }
 
-    public Price(Airline airline, String flightNumber, TicketClass ticketClass, TicketType ticketType,
-            LocalDateTime startDate, LocalDateTime endDate, BigDecimal price, BigDecimal priceLeg1,
-            BigDecimal priceLeg2) {
-        this.priceId = new PriceId(airline, flightNumber, ticketClass, ticketType, startDate);
+    public Price(PriceId priceId, Airline airline, TicketClass ticketClass, TicketType ticketType,
+            LocalDateTime endDate, BigDecimal price, BigDecimal priceLeg1, BigDecimal priceLeg2) {
+        this.priceId = priceId;
+        this.airline = airline;
+        this.airline.addPrice(this);
+        this.ticketClass = ticketClass;
+        this.ticketClass.addPrice(this);
+        this.ticketType = ticketType;
+        this.ticketType.addPrice(this);
         this.endDate = endDate;
         this.price = price;
         this.priceLeg1 = priceLeg1;
--- src/main/java/com/uon/seng3160/group2/flightpub/entity/compositekey/PriceId.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/entity/compositekey/PriceId.java
@@ -3,14 +3,8 @@ package com.uon.seng3160.group2.flightpub.entity.compositekey;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import com.uon.seng3160.group2.flightpub.entity.Airline;
-import com.uon.seng3160.group2.flightpub.entity.TicketType;
-import com.uon.seng3160.group2.flightpub.entity.TicketClass;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.EqualsAndHashCode;
@@ -19,20 +13,14 @@ import lombok.EqualsAndHashCode;
 @Embeddable
 public class PriceId implements Serializable {
 
-    @ManyToOne
-    @JoinColumn(name = "AirlineCode", referencedColumnName = "AirlineCode", nullable = false)
-    private Airline airline;
+    private String airlineCode;
 
     @Column(columnDefinition = "VARCHAR(6)", nullable = false)
     private String flightNumber;
 
-    @ManyToOne
-    @JoinColumn(name = "ClassCode", referencedColumnName = "ClassCode", nullable = false)
-    private TicketClass ticketClass;
+    private String classCode;
 
-    @ManyToOne
-    @JoinColumn(name = "TicketCode", referencedColumnName = "TicketCode", nullable = false)
-    private TicketType ticketType;
+    private String ticketCode;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
@@ -41,12 +29,52 @@ public class PriceId implements Serializable {
     public PriceId() {
     }
 
-    public PriceId(Airline airline, String flightNumber,
-            TicketClass ticketClass, TicketType ticketType, LocalDateTime startDate) {
-        this.airline = airline;
+    public PriceId(String airlineCode, String flightNumber,
+            String classCode, String ticketCode, LocalDateTime startDate) {
+        this.airlineCode = airlineCode;
         this.flightNumber = flightNumber;
-        this.ticketClass = ticketClass;
-        this.ticketType = ticketType;
+        this.classCode = classCode;
+        this.ticketCode = ticketCode;
+        this.startDate = startDate;
+    }
+
+    public String getAirlineCode() {
+        return this.airlineCode;
+    }
+
+    public void setAirlineCode(String airlineCode) {
+        this.airlineCode = airlineCode;
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getClassCode() {
+        return this.classCode;
+    }
+
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
+    }
+
+    public String getTicketCode() {
+        return this.ticketCode;
+    }
+
+    public void setTicketCode(String ticketCode) {
+        this.ticketCode = ticketCode;
+    }
+
+    public LocalDateTime getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 }
