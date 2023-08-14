d4ccf0a642b35072051f67a18b4f128eab252f2a
Robert Duggan <rob.m.duggan@gmail.com>
2023-08-14 16:39:52 +1000

    First Commit: Rob Testing for Flight Search

--- src/main/java/com/uon/seng3160/group2/flightpub/repository/PriceRepository.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/repository/PriceRepository.java
@@ -1,6 +1,6 @@
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-public PriceRepository extends JpaRepository<PriceBean, char>{
+//public PriceRepository extends JpaRepository<PriceBean, char>{
 
-}
+//}
