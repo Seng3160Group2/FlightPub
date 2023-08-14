aff507cff1c62070b79670e22857915ac3b87b8d
Quiescent <fallenartx@gmail.com>
2023-08-14 21:35:53 +1000

    Removed unnecessary files and did slight changes to User

--- src/main/java/com/uon/seng3160/group2/flightpub/entity/Role.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/entity/Role.java
@@ -18,7 +18,7 @@ public class Role
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long roleId;
 
     @Column(nullable=false, unique=true)
     private String name;
