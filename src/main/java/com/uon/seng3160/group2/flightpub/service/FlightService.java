4ad8f38b87a0cb54fc20c0243d4ef57eb24fdc44
Lukeels911 <llendon911@gmail.com>
2023-08-22 07:57:51 +1000

    final search results
    list of destination paths to list of flight routes

--- src/main/java/com/uon/seng3160/group2/flightpub/service/FlightService.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/service/FlightService.java
@@ -7,5 +7,5 @@ import com.uon.seng3160.group2.flightpub.entity.Flight;
 
 public interface FlightService {
     public Optional<Flight> getFlight(String airlineCode, String flightNumber, LocalDateTime departureTime);
-    public Optional<Flight> getByDepartureAndDestination(String departure, String destination);
+    public Optional<Flight> getByDepartureAndDestinationAndStopOver(String departure, String destination, String stopOver);
 }
