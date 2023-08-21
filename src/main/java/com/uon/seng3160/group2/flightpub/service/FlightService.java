3bee088766423d36b7c306025582ef06715b8990
Lukeels911 <llendon911@gmail.com>
2023-08-21 13:01:13 +1000

    commit

---
+++ src/main/java/com/uon/seng3160/group2/flightpub/service/FlightService.java
@@ -0,0 +1,11 @@
+package com.uon.seng3160.group2.flightpub.service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import com.uon.seng3160.group2.flightpub.entity.Flight;
+
+public interface FlightService {
+    public Optional<Flight> getFlight(String airlineCode, String flightNumber, LocalDateTime departureTime);
+    public Optional<Flight> getByDepartureAndDestination(String departure, String destination);
+}
