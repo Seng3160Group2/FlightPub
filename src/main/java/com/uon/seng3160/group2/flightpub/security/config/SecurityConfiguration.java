02732bb8bd2b0e89a97ba51a101d91392fbfbf15
Quiescent <fallenartx@gmail.com>
2023-08-24 13:24:50 +1000

    Fixed GenericRepository.java

--- src/main/java/com/uon/seng3160/group2/flightpub/security/config/SecurityConfiguration.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/security/config/SecurityConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,7 +20,8 @@ import com.uon.seng3160.group2.flightpub.service.impl.infrastructure.FlightBooki
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfiguration {
 
 	@Autowired
--- src/main/java/com/uon/seng3160/group2/flightpub/security/jwt/JwtRequestFilter.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/security/jwt/JwtRequestFilter.java
@@ -29,8 +29,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	private JwtTokenUtil jwtTokenUtil;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		
 		final String requestTokenHeader = request.getHeader("Authorization");
 		
