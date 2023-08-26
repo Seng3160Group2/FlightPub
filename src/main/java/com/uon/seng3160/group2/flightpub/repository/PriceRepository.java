5e8cf332536a43608f7e2e4329bdc3c9810b9a30
Robert Duggan <rob.m.duggan@gmail.com>
2023-08-26 14:45:00 +1000

    Follow up: Return Flight

--- src/main/java/com/uon/seng3160/group2/flightpub/repository/PriceRepository.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/repository/PriceRepository.java
@@ -1,11 +1,12 @@
-package com.uon.seng3160.group2.flightpub.repository;
+//package com.uon.seng3160.group2.flightpub.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.uon.seng3160.group2.flightpub.entity.Price;
-import com.uon.seng3160.group2.flightpub.entity.compositekey.PriceId;
+//public PriceRepository extends JpaRepository<PriceBean, char>{
+//import com.uon.seng3160.group2.flightpub.entity.Price;
+//import com.uon.seng3160.group2.flightpub.entity.compositekey.PriceId;
 
 
-public interface PriceRepository extends JpaRepository<Price, PriceId> {
+//public interface PriceRepository extends JpaRepository<Price, PriceId> {
 
-}
+//}
