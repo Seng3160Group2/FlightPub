3952eaff14c83e9bc8fac012b0514e18370162a1
Anton Lyamin <antoine.lyamin@gmail.com>
2023-09-03 23:48:20 +1000

    Update test controller example

--- src/main/java/com/uon/seng3160/group2/flightpub/controller/TestController.java
+++ src/main/java/com/uon/seng3160/group2/flightpub/controller/TestController.java
@@ -1,22 +1,20 @@
 package com.uon.seng3160.group2.flightpub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
-import java.io.Serializable;
 
-@Slf4j
 @Controller
 public class TestController {
 
     @RequestMapping("test-session")
-    public String getSession(HttpSession session) {
+    public String getSession(HttpSession session, Model model) {
         if (session.getAttribute("visit-counter") == null) {
             session.setAttribute("visit-counter", "1");
-            log.info("New user ");
-        } 
+        }
+        model.addAttribute("session", session);
         return "test-session";
     }
 }
